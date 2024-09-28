104953f73994f53b412922c73b3ecfd35d64597a
xisxus <shefain3@gmail.com>
2024-09-28 18:45:10 +0600

    new

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Edit } from './conponents/Edit'
 import { Footer } from './layers/Footer'
 import { Home } from './conponents/Home'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Details } from './conponents/Details'
+
 function App() { 
   return (<>  <div className='container'>
 <BrowserRouter><Nav/><Routes>
@@ -15,7 +15,7 @@ function App() {
   <Route path='/list' element= {<List/>}/>
   <Route path='/list/add' element= {<Create/>}/>
   <Route path='/list/edit/:id' element={<Edit/>}/>
-  <Route path='/list/details/:id' element={<Details/>}/>
+ 
 </Routes>
 <Footer/></BrowserRouter> </div> </>  )
 }
